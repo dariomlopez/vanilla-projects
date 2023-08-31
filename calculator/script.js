9283cfeab6f9f4f5b818cdb677062a04426b7b66
dariomlopez <135547941+dariomlopez@users.noreply.github.com>
2023-08-31 11:08:31 +0200

    Updated input checkboxes and JS

--- calculator/script.js
+++ calculator/script.js
@@ -1,21 +1,44 @@
-
-
+/**Input where the password is shown */
+const passwordInput = document.getElementById("password-field");
+/** */
+const generateButton = document.getElementById("generate-button");
+/**Range input */
 const lengthInput = document.getElementById("lengthPass");
+/**Span field showing the number of characters */
 const lengthValue = document.getElementById("lengthValue");
+/**Input elements */
+const includeLower = document.getElementById("includeLower");
+const includeUpper = document.getElementById("includeUpper");
+const includeNumbers = document.getElementById("includeNumbers");
+const includeSymbols = document.getElementById("includeSymbols");
 
-const chars = {
-  numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-  symbols: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-","+", "=", "{", "[", "}", "]", ";", ":", "<", ",", ">", ".", "?", "/"],
-  upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-  "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-  "W", "X", "Y", "Z"],
-  lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-  "w", "x", "y", "z"]
-};
-//console.log(Object.keys(chars.numbers).length);
+/**Array of symbols */
+const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-","+", "=", "{", "[", "}", "]", ";", ":", "<", ",", ">", ".", "?", "/"];
+/** Getting one symbol */
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+ 
+function getLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+  
+
 
 /** Update the value of the length */
 lengthInput.addEventListener("input", () => {
   lengthValue.textContent = lengthInput.value; 
+});
+
+generateButton.addEventListener("click", () => {
+  const password = generatePassword();
+  passwordInput.value = password;
 });
